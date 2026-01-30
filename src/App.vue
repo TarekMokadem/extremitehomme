190f4a419743372a572fb2151c94409e6f244810
Tarek Mokadem <tarek.mokadem@student.junia.com>
2026-01-30 13:23:54 +0100

    fix: simplification router-view sans transition

--- src/App.vue
+++ src/App.vue
@@ -48,21 +48,6 @@ onMounted(async () => {
     <AppHeader />
 
     <!-- Contenu de la page (router) -->
-    <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
-    </router-view>
+    <router-view />
   </div>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.15s ease;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
